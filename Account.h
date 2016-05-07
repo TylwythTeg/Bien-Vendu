5c103e533e5d1ecd2067d5970de1cc91ba49fd26
TylwythTeg <garciarobertoiv@yahoo.com>
2016-05-07 02:46:20 -0700

    Update Account.h

--- Account.h
+++ Account.h
@@ -5,18 +5,35 @@
 class Account
 {
 	public:
-		Account(string str){ name = str; }
+		Account(string str)
+		{ 
+			name = str; 
+
+			int m, d, y;
+			double value;
+			if (cashLogFile(name + ".CashLog"))
+			{
+				vector<string>fields = parseFile(name + ".CashLog");	//functionize this
+				vector<Log> entries;
+
+				for (int i = 0; i < fields.size; ++i)
+					entries.push_back(getEntryFromString(fields[i]));
+			}
+		
+		}
 
 		string getName() { return name; }
 		int getNumOfMachines() { return numOfMachines; }
 
 		void setNumOfMachines(int n) { numOfMachines = n; }
 
+		
+
 
 	private:
 		string name;
 		int numOfMachines;
-		vector<Log> Entries;
+		vector<Log> entries;
 };
 
 struct Date
@@ -29,7 +46,7 @@ struct Date
 class Log
 {
 	public:
-		Log() {}
+		Log() { accountName = ""; }
 		Log(string str) { accountName = str; }
 
 		string getAccountName() { return accountName; }
@@ -40,12 +57,19 @@ class Log
 		int getDay() { return date.day; }
 		int getYear() { return date.year; }
 
+		string getDateString()
+		{
+			return (to_string(date.month) + "/" + to_string(date.day) + "/" + to_string(date.year));
+		}
+
 		void setDate(int m, int d, int y)
 		{
 			date.month = m;
 			date.day = d;
 			date.year = y;
 		}
+		void setValue(double doub) { value = doub; }
+		void setAccountName(string str) { accountName = str; }
 
 	private:
 		string accountName;
