40ac623ecc088af48dcdfeaff72732f253cd0b55
TylwythTeg <garciarobertoiv@yahoo.com>
2016-05-12 19:04:04 -0700

    Update Date.h

--- Date.h
+++ Date.h
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <string>
+#include <map>
 using namespace std;
 
 
@@ -137,4 +138,77 @@ bool isDate(Date date)
 		break;
 	}
 
+}
+
+int daysBetween(const Date& lateDate, const Date& earlyDate)	//Find amount of days beetween two cash logs
+{
+	int daystosubtract = 0;
+	int totaldays = 0;
+
+	map<string, int>Days;
+	Days["jan"] = 31;
+	Days["feb"] = 28;
+	Days["mar"] = 31;
+	Days["apr"] = 30;
+	Days["may"] = 31;
+	Days["jun"] = 30;
+	Days["jul"] = 31;
+	Days["aug"] = 31;
+	Days["sep"] = 30;
+	Days["oct"] = 31;
+	Days["nov"] = 30;
+	Days["dec"] = 31;
+
+	if ((lateDate.month == earlyDate.month) && (lateDate.year == earlyDate.year))
+	{
+		return lateDate.day - earlyDate.day;	//mo and day are same so return simple difference of days
+	}
+
+	if (earlyDate.day > lateDate.day)	//like if earlylog is 3/8/2016 and latelog is 7/5/2016 (partial month)
+	{
+		daystosubtract = earlyDate.day - lateDate.day;	//subtract these days later
+														//will act as if LateLog.GetDay==EarlyLog.GetDay
+	}
+
+	int monthsbetween = lateDate.month - earlyDate.month;	//4
+	int currentmonth = earlyDate.month;	//gonna go from earlylog to latelog
+
+										//yearsbetween
+	int yearsbetween = lateDate.year - earlyDate.year;
+	monthsbetween = monthsbetween + (yearsbetween * 12);
+	//yearsbetween
+
+	string monthstring = monthStringFromInt(currentmonth);
+
+	int currentyear = earlyDate.year;
+
+	//Month newmonth;
+
+	for (int i = 0; i <monthsbetween; ++i)
+	{
+		if (i == 0)	//if first month we must subtract days from out starting point
+		{
+			totaldays += (Days[monthstring] - earlyDate.day); //31-8 = 23 total days
+		}
+		if (i != 0)
+		{
+			totaldays += (Days[monthstring]);
+		}
+
+		if ((monthstring == "feb") && leapYear(currentyear))
+			totaldays++;
+
+		monthstring = monthStringFromInt(earlyDate.month + (i + 1));	//monthstring is now string of month + (iterations+1)															
+
+		if (monthstring == "jan")
+			currentyear++;
+	}
+	//ran through up to beginning of LateLog.GetMonth()
+	totaldays += earlyDate.day;
+	//now we have found the days between for perfect months
+
+	//subtract daystosubtract
+	totaldays -= daystosubtract;
+
+	return totaldays;
 }
