ec41723c7d6a3b561822b86678acafe51bd69efb
TylwythTeg <garciarobertoiv@yahoo.com>
2016-05-12 18:42:31 -0700

    Update BienVendu.cpp

--- BienVendu.cpp
+++ BienVendu.cpp
@@ -7,42 +7,18 @@
 #include <vector>
 #include <cstdio>
 
-const string MSTR_ACNT_LIST = "Accounts.txt";
 
+void menuMain();
 void viewEditAccounts();
 void menuNewAccount();
 void menuEditAccount(string account);
 void menuCreateCashLog(string account);
 void menuEditCashLog(string account);
 void menuAddEntries(string account);
-void menuMain();
 void menuDeleteAccount(string account);
 void menuDeleteByDate(string account);
 void askToCreate();
 void askForAccount();
-
-//util
-bool account(string account);
-bool masterFile();
-bool accounts();
-bool isDate(Date date);
-bool isDate(int month, int day, int year);
-bool leapYear(int year);
-bool logDateExists(Date date, vector<Log> log);
-Account buildAccountFromFile(string file);
-void printAccounts();
-void printLogs(string account);
-void analyzeCashLog(string account);
-int daysBetween(Log lateLog, Log earlyLog);
-void deleteAccount(string account);
-void deleteLogEntry(Log userEntry);
-void removeFromMaster(string account);
-void writeAccountFile(Account newAccount);
-void writeLogFile(string account, vector<Log>Log);
-void writeMasterFile(string accountName);
-
-
-
 
 int main()
 {
@@ -260,7 +236,7 @@ void menuCreateCashLog(string account)
 	double doub = 0.0;
 	while (cin >> st >> doub) //terminate?
 	{
-		Log userEntry = getEntryFromString(st);
+		Log userEntry = getLogFromString(st);
 
 		if (!isDate(userEntry.getDate()))
 		{
@@ -382,7 +358,7 @@ void menuAddEntries(string account)
 	//save when done
 	
 
-	vector<Log> log = getEntriesFromFile(account + ".CashLog");
+	vector<Log> log = getLogsFromFile(account + ".CashLog");
 	int fileEntriesNum = log.size();
 
 	cout << "Enter entries as ##/##/#### ####.##\n";
@@ -391,7 +367,7 @@ void menuAddEntries(string account)
 	double value;
 	while (std::cin >> st >> value)
 	{
-		Log userEntry = getEntryFromString(st);
+		Log userEntry = getLogFromString(st);
 		userEntry.setValue(value);
 
 		if (!isDate(userEntry.getDate()))
@@ -448,312 +424,12 @@ void menuAddEntries(string account)
 	}
 }
 
-//util
-bool masterFile()
-{
-	if (!(ifstream(MSTR_ACNT_LIST).good()))
-		return false;
-
-	return true;
-}
-bool account(string account)
-{
-	if (!masterFile())
-		return false;
-
-	vector<string>fields = parseFile(MSTR_ACNT_LIST);
-
-	for (int i = 0; i < fields.size(); ++i)
-	{
-		if (fields[i] == account)
-			return true;
-	}
-	return false;
-}
-
-bool accounts()
-{
-	vector<string>accounts = parseFile(MSTR_ACNT_LIST);
-
-	if (accounts.empty())
-		return false;
-
-	return true;
-}
-
-bool isDate(Date date)
-{
-
-	switch (date.month)
-	{
-		case 1:
-		case 3:
-		case 5:
-		case 7:
-		case 8:
-		case 10:
-		case 12:
-			if (date.day >= 1 && date.day <= 31)
-				return true;
-			return false;
-			break;
-		case 4:
-		case 6:
-		case 9:
-		case 11:
-			if (date.day >= 1 && date.day <= 30)
-				return true;
-			return false;
-			break;
-		case 2:
-			if ( (date.day <= 28) || ( date.day == 29 && leapYear(date.year) ) )
-				return true;
-			return false;
-			break;
-		default:
-			return false;
-			break;
-	}
-
-}
-
-bool isDate(int month, int day, int year)
-{
-
-	switch (month)
-	{
-	case 1:
-	case 3:
-	case 5:
-	case 7:
-	case 8:
-	case 10:
-	case 12:
-		if (day >= 1 && day <= 31)
-			return true;
-		return false;
-		break;
-	case 4:
-	case 6:
-	case 9:
-	case 11:
-		if (day >= 1 && day <= 30)
-			return true;
-		return false;
-		break;
-	case 2:
-		if ((day <= 28) || (day == 29 && leapYear(year)))
-			return true;
-		return false;
-		break;
-	default:
-		return false;
-		break;
-	}
-
-}
-
-bool leapYear(int year)
-{
-	if (year % 4 == 0)
-	{
-		if (year % 100 == 0)
-		{
-			if (year % 400 == 0)
-			{
-				return true;
-			}
-			return false;
-		}
-		return true;
-	}
-	return false;
-}
-
-bool logDateExists(Date date, vector<Log> log) //dateExistsInLog logEntryExists
-{
-
-	for (int i = 0; i < log.size(); ++i)
-	{
-		if ((log[i].getMonth() == date.month) && (log[i].getDay() == date.day) && (log[i].getYear() == date.year))
-			return true;
-	}
-	return false;
-}
-
-Account buildAccountFromFile(string file)
-{
-	Vector<string>fields = parseFile(file);
-
-	Account acnt(fields[0]);
-	acnt.setNumOfMachines(atoi(fields[1].c_str()));
-	return acnt;
-}
-
-void printAccounts()
-{
-	vector<string>accounts = parseFile(MSTR_ACNT_LIST);
-
-	cout << "\nAccounts List: \n\n";
-
-	for (int i = 0; i < accounts.size(); ++i)
-	{
-		Account acnt = buildAccountFromFile(accounts[i] + ".txt");  //Build account from .txt...for instance one loop pass through will build Atherton from Atherton.txt
-		cout << "\t" << acnt.getName() << " has " << acnt.getNumOfMachines() << " machines\n";
-	}
-
-	cout << endl;
-}
-
-void printLogs(string account)
-{
-	vector<string>fields = parseFile(account + ".CashLog");
-
-	for (int i = 0; i < fields.size(); ++i)
-	{
-		cout << "Entry [" << i << "]: " << fields[i] << endl;
-
-	}
-}
-
-void analyzeCashLog(string account)
-{
-	//calculate average daily earnings        
-	//and then total average
-
-	vector<Log>log;
-	vector<string>fields = parseFile(account + ".CashLog");
-	vector<double>dailyValue;
-	for (int i = 0; i < fields.size(); ++i)
-	{
-		Log entry = getEntryFromString(fields[i]);
-
-		log.push_back(entry);
-
-		cout << "\t" << log[i].getMonth() << "/" << log[i].getDay() << "/" << log[i].getYear() << ": $" << log[i].getValue() << endl;
-
-		///////////Calculate days between and money made per day/////////
-		if (i == 0)		//if first Log entry skip for loop iteration
-		{
-			cout << endl;
-			continue;
-		}
-
-		int daysbetween = daysBetween(log[i], log[i - 1]);
-
-		cout << "\tDays between last log date: " << daysbetween << endl;
-		cout << "\tThe money made per day was " << log[i].getValue() / daysbetween << endl << endl;
-
-		dailyValue.push_back(daysbetween);
-		///////////Calculate days between and money made per day//////////
-	}
-
-	//average daily
-	
-	double sum = 0;
-	for (int i = 0; i < dailyValue.size(); ++i)
-		sum += dailyValue[i];
-
-	cout << "The average money made per day is " << sum / dailyValue.size() << endl << endl;
-
-	return;
-}
-
-int daysBetween(Log lateLog, Log earlyLog)	//Find amount of days beetween two cash logs
-{
-	int daystosubtract = 0;
-	int totaldays = 0;
-
-	map<string, int>Days;
-	Days["jan"] = 31;
-	Days["feb"] = 28;
-	Days["mar"] = 31;
-	Days["apr"] = 30;
-	Days["may"] = 31;
-	Days["jun"] = 30;
-	Days["jul"] = 31;
-	Days["aug"] = 31;
-	Days["sep"] = 30;
-	Days["oct"] = 31;
-	Days["nov"] = 30;
-	Days["dec"] = 31;
-
-	if ((lateLog.getMonth() == earlyLog.getMonth()) && (lateLog.getYear() == earlyLog.getYear()))
-	{
-		return lateLog.getDay() - earlyLog.getDay();	//mo and day are same so return simple difference of days
-	}
-
-	if (earlyLog.getDay() > lateLog.getDay())	//like if earlylog is 3/8/2016 and latelog is 7/5/2016 (partial month)
-	{
-		daystosubtract = earlyLog.getDay() - lateLog.getDay();	//subtract these days later
-																//will act as if LateLog.GetDay==EarlyLog.GetDay
-	}
-
-	int monthsbetween = lateLog.getMonth() - earlyLog.getMonth();	//4
-	int currentmonth = earlyLog.getMonth();	//gonna go from earlylog to latelog
-
-											//yearsbetween
-	int yearsbetween = lateLog.getYear() - earlyLog.getYear();
-	monthsbetween = monthsbetween + (yearsbetween * 12);
-	//yearsbetween
-
-	string monthstring = monthStringFromInt(currentmonth);
-
-	int currentyear = earlyLog.getYear();
-
-	//Month newmonth;
-
-	for (int i = 0; i <monthsbetween; ++i)
-	{
-		if (i == 0)	//if first month we must subtract days from out starting point
-		{
-			totaldays += (Days[monthstring] - earlyLog.getDay()); //31-8 = 23 total days
-		}
-		if (i != 0)
-		{
-			totaldays += (Days[monthstring]);
-		}
-
-		if ((monthstring == "feb") && leapYear(currentyear))
-			totaldays++;
-
-		monthstring = monthStringFromInt(earlyLog.getMonth() + (i + 1));	//monthstring is now string of month + (iterations+1)															
-
-		if (monthstring == "jan")
-			currentyear++;
-	}
-	//ran through up to beginning of LateLog.GetMonth()
-	totaldays += earlyLog.getDay();
-	//now we have found the days between for perfect months
-
-	//subtract daystosubtract
-	totaldays -= daystosubtract;
-
-	return totaldays;
-}
-
-void deleteAccount(string account)
-{
-	if (remove((account + ".txt").c_str()) != 0) 
-	{
-	 
-		cout << "Failed to delete " << account << "..." << endl;
-		return;
-	}
-	else
-		cout << account << " file deleted\n";
-
-	remove((account + ".CashLog").c_str());
-
-	removeFromMaster(account);
-}
-
 void deleteLogEntry(Log userEntry)
 {
 	const string CSH_LOG = userEntry.getAccountName() + ".CashLog";
 
 	vector<string>fields = parseFile(CSH_LOG);
-	vector<Log> log = getEntriesFromFile(CSH_LOG);
+	vector<Log> log = getLogsFromFile(CSH_LOG);
 
 	for (int i = 0; i < log.size(); ++i)
 	{
@@ -808,7 +484,7 @@ void menuDeleteByDate(string account)
 	cout << "Enter date as ##/##/#### to delete entry\n";
 	const string CSH_LOG = account + ".CashLog";
 
-	vector<Log>log = getEntriesFromFile(CSH_LOG);
+	vector<Log>log = getLogsFromFile(CSH_LOG);
 
 	string st;
 	while (cin >> st)
@@ -816,7 +492,7 @@ void menuDeleteByDate(string account)
 		if (cin.eof())
 			return;
 
-		Log userEntry = getEntryFromString(st);
+		Log userEntry = getLogFromString(st);
 		userEntry.setAccountName(account);
 
 		if (logDateExists(userEntry.getDate(), log))
@@ -831,174 +507,6 @@ void menuDeleteByDate(string account)
 	if (cin.eof())
 		return;
 
-}
-
-void removeFromMaster(string account)
-{
-	/*
-	Delete entry in master account file
-	by loading entries into vector
-	deleting appropriate entry, rewriting to temp.txt
-	delete master account file, and rename temp.txt to master account file name
-	*/
-	vector<string>fields = parseFile(MSTR_ACNT_LIST);
-
-	ofstream new_master("temp.txt");
-	for (int i = 0; i < fields.size(); ++i)
-	{
-		if (fields[i] == account)
-			fields.erase(fields.begin() + i);
-	}
-
-	if (!new_master)
-	{
-		cout << "Error updating master account file. Account not fully deleted.\n";
-		return;
-	}
-
-	for (int i = 0; i < fields.size(); ++i)
-	{
-		if (!new_master)
-		{
-			cout << "Error updating master account file. Account not deleted.\n" << endl;
-			return;
-		}
-		else
-		{
-			new_master << fields[i] << endl;
-		}
-	}
-	new_master.close();
-	remove(MSTR_ACNT_LIST.c_str());
-	rename("temp.txt", MSTR_ACNT_LIST.c_str());
-}
-
-void writeAccountFile(Account newAccount)
-{
-	ofstream writer(newAccount.getName() + ".txt");
-
-	if (!writer)
-	{
-		cout << "Error opening file\n" << endl;
-		return;
-	}
-
-	//Account Name, Number of Machines, etc
-	writer << newAccount.getName() << "\n" << newAccount.getNumOfMachines() << "\n";
-	writer.close();
-	cout << newAccount.getName() << " saved\n";
-
-	writeMasterFile(newAccount.getName());
-
-
-}
-
-void writeLogFile(string account, vector<Log>log)
-{
-	const string CSH_LOG = account + ".CashLog";
-
-	//sort logs by date
-	if (log.size() > 1)
-	{
-		sort(log.begin(), log.end());
-	}
-
-	//log file already exists
-	if (cashLogFile(account))
-	{
-		//write to temp, delete account + ".CashLog", rename temp to account + ."CashLog"
-		ofstream new_cashlog("templog.txt");
-
-		if (!new_cashlog)
-		{ 
-			cout << "Failed to save logfile. Aborting without saving...\n";
-			return;
-		}
-		else
-		{
-			for (int i = 0; i < log.size(); ++i)
-			{
-				new_cashlog << log[i].getDateString() << " " << log[i].getValue() << "\n";
-			}
-
-			//now delete account.cashlog and replace with temp
-			if (remove((account + ".CashLog").c_str()) != 0)
-			{
-				cout << "Failed to update cashlog file. Aborting without saving...\n" << endl;
-				return;
-			}
-			else
-			{
-				new_cashlog.close();
-				rename("templog.txt", CSH_LOG.c_str());
-				cout << "New entries saved\n";
-				return;
-			}
-		}
-
-	}
-
-
-	//logfile doesn't exist
-	ofstream logfile(CSH_LOG);
-
-	if (!logfile)
-	{
-		cout << "Error opening file. Aborting without saving...\n" << endl;
-	}
-	else
-	{
-		for (int i = 0; i < log.size(); ++i)
-		{
-			//save and print
-
-			if (!logfile)
-			{
-				cout << "Error opening file. Aborting without saving...\n" << endl;
-				return;
-			}
-			else
-			{
-				//Account Name, Number of Machines, etc
-				logfile << log[i].getDateString() << " " << log[i].getValue() << "\n";
-				cout << "Entry " << i << ": " << log[i].getDateString() << " " << log[i].getValue() << " saved\n";
-			}
-			
-		}
-		cout << "All entries saved...\n";
-		logfile.close();
-	}
-}
-
-void writeMasterFile(string accountName)
-{
-
-	if (masterFile())
-	{
-		ofstream masterlist(MSTR_ACNT_LIST, ios::app);
-		if (!masterlist)
-		{
-			cout << "Error opening file" << endl;
-			return;
-		}
-
-		masterlist << accountName << "\n";
-		masterlist.close();
-		cout << "Master accounts list saved\n";
-	}
-	else
-	{ 
-		ofstream masterlist(MSTR_ACNT_LIST);
-		if (!masterlist)
-		{
-			cout << "Error opening file" << endl;
-			return;
-		}
-
-		masterlist << accountName << "\n";
-		masterlist.close();
-		cout << "Master accounts list saved\n";
-	}
 }
 
 
